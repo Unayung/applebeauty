b86c1eb1a3f00203d2f0c8b909471b9dfbbca641
unayung <Unayung@gmail.com>
2012-11-28 10:58:15 +0800

    seo

--- config/initializers/seo_helper.rb
+++ config/initializers/seo_helper.rb
@@ -2,7 +2,7 @@
 SeoHelper.configure do |config|
   config.skip_blank               = false
   config.site_name = Setting.app_name
-  config.default_page_description = "PAGE_DESSCRIPTION"
+  config.default_page_description = Setting.description
   config.default_page_keywords    = "PAGE_KEYWORDS"
   config.default_page_image = Setting.domain + Setting.default_logo_url
   config.site_name_formatter  = lambda { |title, site_name|   "#{title} « #{site_name}".html_safe }
