7da3b32acd7655f4944fb5cb55040ad6d53afc0d
Unayung Chen <unayung@gmail.com>
2016-04-01 13:58:37 +0800

    config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,14 +10,14 @@ Applebeauty::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
-
+  config.serve_static_files = false
+  config.eager_load = true
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
-
+  config.log_level = :info
   # Generate digests for assets URLs
   config.assets.digest = true
 
