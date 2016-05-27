b055dc184779e2ffa07480d8c4b8137793cf0956
Unayung Chen <unayung@gmail.com>
2016-05-28 03:46:54 +0800

    token

--- config/initializers/messenger_bot.rb
+++ config/initializers/messenger_bot.rb
@@ -1,5 +1,5 @@
 Messenger::Bot.config do |config|
-  config.access_token = "EAAQ1okPGMWYBAM0AisGcfL2aYTDdrMayHI6GBYOIDfFagEQrOxcZBdbEdYPWCaFvi6DuEdkKp1lspNj96v5C99fCf3VRttnXvwOyIZBVMO4taZBKIGfVkEGtEXmzQ2DkaKl0ZAY47AMy8ZC5yk1luCNus6AFtu8WZCg7tBCFwZATwZDZD"
+  config.access_token = "EAAQ1okPGMWYBAFefpPdZA2bZBWUNOjvJsaeyXXo3sZBC6pJQfnfUQxJYDFaUCVmlrFL7OMSjSwxIqZAvsPaGA7QbWkaPl0vb9jRFlnhNflzCyTZChKKIiHD2GKQJYx747ZBO3U5M69F99GnO8T5q3D6fQUMByIW1EobnwaT0GhJgZDZD"
   config.validation_token = "aabbccdd"
   config.secret_token = "bbcbfd0284d436c0fb4bbb1bc43db584"
 end
