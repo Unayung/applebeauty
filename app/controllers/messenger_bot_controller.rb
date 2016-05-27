853697be89cc811ce735c8db9ba133328732d92b
Unayung Chen <unayung@gmail.com>
2016-05-28 03:24:07 +0800

    try button

--- app/controllers/messenger_bot_controller.rb
+++ app/controllers/messenger_bot_controller.rb
@@ -8,6 +8,38 @@ class MessengerBotController < ActionController::Base
     elsif event['message']['text'] == "link"
       link = Link.last
       sender.reply({ text: link.title + " / " + link.url })
+    elsif event['message']['text'] == "button"
+      sender.reply({
+        "attachment": {
+              "type": "template",
+              "payload": {
+                "template_type": "generic",
+                "elements": [{
+                  "title": "First card",
+                  "subtitle": "Element #1 of an hscroll",
+                  "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+                  "buttons": [{
+                    "type": "web_url",
+                    "url": "https://www.messenger.com/",
+                    "title": "Web url"
+                  }, {
+                    "type": "postback",
+                    "title": "Postback",
+                    "payload": "Payload for first element in a generic bubble",
+                  }],
+                },{
+                  "title": "Second card",
+                  "subtitle": "Element #2 of an hscroll",
+                  "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+                  "buttons": [{
+                    "type": "postback",
+                    "title": "Postback",
+                    "payload": "Payload for second element in a generic bubble",
+                  }],
+                }]
+              }
+            }
+        })
     end
   end
 
