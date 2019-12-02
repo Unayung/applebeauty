c7125aec5a5614d23e3aa99491ba03a77448e291
Chen Chia Yang <yung.chen@oakda.io>
2019-12-03 01:20:26 +0800

    fix location replace

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,5 +11,6 @@ class PagesController < ApplicationController
     Rails.logger.info("name: #{name}")
     Rails.logger.info("liff_uid: #{liff_uid}")
     Rails.logger.info("-----")
+    render layout: false
   end
 end
